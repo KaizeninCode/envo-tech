0156f81454db5a6678abd19375545412c6f86886
KaizeninCode <philwekullo@gmail.com>
2025-04-04 15:58:16 +0300

    Fix Home Page text layout

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,16 +7,15 @@ const HomePage = () => {
   const handleFindClick = () => router.push("/our-services");
   const handleContactClick = () => router.push("/contact");
   return (
-    <section className="flex-1 lg:p-8 p-4 bg-[url(/hero1.png)] bg-center bg-cover min-h-screen">
+    <section className="flex-1 flex justify-center items-center lg:p-8 p-4 bg-[url(/hero1.png)] bg-center bg-cover min-h-screen">
       <div className="absolute inset-0 bg-black opacity-70 z-0 max-md:h-[115.9%] lg:h-[111%]" />
-      <div className="relative grid lg:grid-cols-2 items-center max-lg:mt-24">
-        <div className="mx-auto mt-48">
-          <h1 className="lg:text-5xl text-xl font-bold">
+        <div className="mx-auto relative flex flex-col items-center justify-center lg:w-4/5 md:p-4">
+          <h1 className="lg:text-6xl md:text-4xl text-xl font-bold text-center">
             We are committed to protecting, developing and managing our most
             precious natural resource,{" "}
             <span className="text-sky-400 font-extrabold">water</span>.
           </h1>
-          <div className="flex justify-start gap-10 lg:w-3/5 mt-8 lg:text-lg text-sm">
+          <div className="flex justify-center gap-10 lg:w-4/5 lg:mx-auto mt-8 lg:text-lg text-sm">
             <button
               className="bg-sky-600 rounded-lg p-2 transition duration-500 ease-in-out cursor-pointer hover:bg-white hover:text-sky-600"
               onClick={handleFindClick}
@@ -31,7 +30,6 @@ const HomePage = () => {
             </button>
           </div>
         </div>
-      </div>
     </section>
   );
 };
